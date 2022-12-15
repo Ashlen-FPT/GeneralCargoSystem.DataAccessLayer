f3d655a8db8346bc8102a77a6871eff584db2f4b
Ashlen-FPT <107978815+Ashlen-FPT@users.noreply.github.com>
2022-12-15 08:05:24 +0200

    Updated Migrations

---
+++ Migrations/20221214075649_UpdatedGCTBL.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneralCargoSystem.Data.Migrations
+{
+    public partial class UpdatedGCTBL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Trade",
+                table: "GCBookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Trade",
+                table: "GCBookings");
+        }
+    }
+}
